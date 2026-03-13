a08bd8403608691acc765ad70516bde84033eb89
blackbird77ad <blackbird77ad@gmail.com>
2026-03-13 21:12:44 +0000

    Fix mongo crash

--- Backend/config/db.mjs
+++ Backend/config/db.mjs
@@ -1,17 +1,11 @@
 import mongoose from 'mongoose';
-
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log('? MongoDB Connected: ' + conn.connection.host);
   } catch (error) {
-    console.error(`❌ MongoDB connection failed: ${error.message}`);
-    process.exit(1); // exit the process if DB fails
+    console.error('? MongoDB connection failed: ' + error.message);
+    process.exit(1);
   }
 };
-
 export default connectDB;
